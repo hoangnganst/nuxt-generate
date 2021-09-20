327997eb3bc494a0ec46ed933efe29dc59f582a6
hoangnganst <nguyenhoangnganst@gmail.com>
2021-09-20 15:03:31 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,10 +18,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
-
+  css: ["ant-design-vue/dist/antd.css"],
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ["@/plugins/antd-ui"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
